d449e35e9a3c48113b66f1a110bd60e261eccac3
Emre Ilhan <emrilhan@gmail.com>
2022-10-21 17:28:04 +0300

    second commit

--- js/todo.js
+++ js/todo.js
@@ -3,19 +3,31 @@
 
 let listDOM = document.querySelector('#list')
 let input = document.querySelector('#task')
+let liNode = document.querySelectorAll('li button')
+// console.log(liNode)
+liNode[0].addEventListener('click', removeElement)
+// addEventListenerList(liNode, 'click', removeElement)
 
 
 // Yeni element ekleme fonksiyonu
 
 
 function newElement() {
-  let liDOM = document.createElement('li')
-  liDOM.innerHTML = input.value + `<button type="button" class="close" aria-label="Close">
-  <span onclick="removeElement()" aria-hidden="true">&times;</span>
+  let newItem = document.createElement('li')
+  newItem.innerHTML = input.value + `<button type="button" class="close" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
 </button>`
-  listDOM.append(liDOM)
+  listDOM.append(newItem)
 }
 
 function removeElement() {
-  listDOM.remove('li')
+  let nodeId = docuemnt.querySelector(liNode[this.id])
+  console.log(nodeId)
+  // listDOM.remove(nodeId)
 }
+
+/* function addEventListenerList(list, event, fn) {
+  for (var i = 0; i < list.length; i++) {
+    list[i].addEventListener(event, fn, false);
+  }
+} */
