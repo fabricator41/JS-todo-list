13ae33ff9f4755039d4433f74777b5ae3249e6f3
Emre Ilhan <emrilhan@gmail.com>
2022-10-22 15:51:54 +0300

    finished

--- js/todo.js
+++ js/todo.js
@@ -7,8 +7,16 @@ let liNode = document.querySelectorAll('li')
 let closeButton = document.querySelectorAll('li button')
 let done = document.querySelectorAll('li')
 
+
+
+
+
+// event atama
+
 addEventListenerList(closeButton, 'click', removeNode)
 addEventListenerList(done, 'click', doneFunc)
+
+// her bir node'a event ekleyen fonksiyon
 
 function addEventListenerList(list, event, fn) {
   for (var i = 0; i < list.length; i++) {
@@ -18,23 +26,39 @@ function addEventListenerList(list, event, fn) {
 
 // Yeni element ekleme fonksiyonu
 
-
-
 function newElement() {
-  let newItem = document.createElement('li')
-  newItem.innerHTML = input.value + `<button type="button" class="close" aria-label="Close">
+  if (input.value !== "") {
+    let newItem = document.createElement('li')
+    newItem.innerHTML = input.value + `<button type="button" class="close" aria-label="Close">
   <span aria-hidden="true">&times;</span>
 </button>`
-  listDOM.append(newItem)
-  addEventListenerList(newItem.childNodes, 'click', removeNode)
-  newItem.addEventListener('click', doneFunc)
+    listDOM.append(newItem)
+    addEventListenerList(newItem.childNodes, 'click', removeNode)
+    newItem.addEventListener('click', doneFunc)
+    document.querySelector('#liveToastSuccess').classList.replace('hide', 'show')
+    setTimeout(closeToast, 3000)
+  }
+  else {
+    document.querySelector('#liveToastError').classList.replace('hide', 'show')
+    setTimeout(closeToast, 3000)
+  }
 }
+
+// element silme fonksiyonu
 
 function removeNode() {
   this.parentElement.remove()
 }
 
+// yapıldı fonksiyonu
+
 function doneFunc() {
-  this.classList.add('checked')
+  this.classList.toggle('checked')
 }
 
+
+// toast otomatik kapanma fonksiyonu
+function closeToast() {
+  document.querySelector('#liveToastSuccess').classList.replace('show', 'hide'),
+    document.querySelector('#liveToastError').classList.replace('show', 'hide')
+}
